be41cd173cc35facf7001c1c6d80fba24924f549
esuau <evan.suau@etu.u-pec.fr>
2019-05-31 00:46:46 +0200

    feat(books): Return a list of Book entities

---
+++ src/main/kotlin/io/esuau/definition/Book.kt
@@ -0,0 +1,3 @@
+package io.esuau.definition
+
+data class Book(val id: Long, val title: String, val author: String, val publisher: String, val year: Int, val isbn: String)
--- src/main/kotlin/io/esuau/rest/BookResource.kt
+++ src/main/kotlin/io/esuau/rest/BookResource.kt
@@ -1,14 +1,19 @@
 package io.esuau.rest
 
+import io.esuau.definition.Book
+import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
 @Path("/books")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 class BookResource {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    fun hello() = "hello"
+    fun getBooks() = listOf(Book(1L, "Clean Architecture: A Craftsman's Guide to Software Structure and Design", "Robert C. Martin", "Prentice Hall", 2017, "978-0134494166"),
+                            Book(1L, "Domain-Driven Design: Tackling Complexity in the Heart of Software", "Eric Evans", "Addison Wesley", 2003, "978-0321125217"))
+
 }
