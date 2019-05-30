54a319e87f19fdc22cb503470e3dd7e5b07fb54c
esuau <evan.suau@etu.u-pec.fr>
2019-05-31 01:11:32 +0200

    feat(book): Return a book by its ID

--- src/main/kotlin/io/esuau/rest/BookResource.kt
+++ src/main/kotlin/io/esuau/rest/BookResource.kt
@@ -1,19 +1,29 @@
 package io.esuau.rest
 
 import io.esuau.definition.Book
-import javax.ws.rs.Consumes
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import java.lang.Exception
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/books")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 class BookResource {
 
+    companion object {
+        var books: MutableList<Book> = mutableListOf(Book(1L, "Clean Architecture: A Craftsman's Guide to Software Structure and Design", "Robert C. Martin", "Prentice Hall", 2017, "978-0134494166"),
+                                                     Book(2L, "Domain-Driven Design: Tackling Complexity in the Heart of Software", "Eric Evans", "Addison Wesley", 2003, "978-0321125217"))
+    }
+
     @GET
-    fun getBooks() = listOf(Book(1L, "Clean Architecture: A Craftsman's Guide to Software Structure and Design", "Robert C. Martin", "Prentice Hall", 2017, "978-0134494166"),
-                            Book(1L, "Domain-Driven Design: Tackling Complexity in the Heart of Software", "Eric Evans", "Addison Wesley", 2003, "978-0321125217"))
+    fun getBooks() = books
+
+    @GET
+    @Path("/{id}")
+    fun getBookById(@PathParam("id") id: Long): Response {
+        val book: Book = books.find { it.id == id } ?: return Response.status(Response.Status.NOT_FOUND).entity("ERROR: Book does not exist").build()
+        return Response.ok(book).build()
+    }
 
 }
